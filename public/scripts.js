e479498f8363ce08892784699c54157ec0e1eea8
margauxwithanx <margaux@margauxwithanx.com>
2015-10-26 16:43:07 -0700

    models, schemas, routes

---
+++ public/scripts.js
@@ -0,0 +1,13 @@
+// /public/scripts.js
+$(function() {
+	$('#log-in').click(function() { 
+		$('#log-in-menu').toggleClass('visible');
+	});	
+});
+
+$(function() {
+	$('#sign-up').click(function() { 
+		$('#sign-up-menu').toggleClass('visible');
+	});	
+});
+
